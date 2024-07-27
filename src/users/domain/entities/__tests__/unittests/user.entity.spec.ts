0573989e6bea378df277b1e33d23a9a89efa2195
Luís Farias <luisgmfarias@gmail.com>
2024-07-26 22:17:54 -0300

    feat: adding class validator fields and validator fields interface

--- src/users/domain/entities/__tests__/unittests/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unittests/user.entity.spec.ts
@@ -1,4 +1,4 @@
-import { UserProps } from './../../user.entity'
+import { UserProps } from '../../user.entity'
 import { UserEntity } from '../../user.entity'
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 
@@ -23,6 +23,19 @@ describe('UserEntity unit tests', () => {
     expect(typeof sut.props.name).toBe('string')
   })
 
+  it('Setter of Name field', () => {
+    const name = 'some name'
+    sut['name'] = name
+    expect(sut.props.name).toEqual(name)
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Update Name field', () => {
+    const name = 'some name'
+    sut.updateName(name)
+    expect(sut.props.name).toEqual(name)
+  })
+
   it('Getter of Email field', () => {
     expect(sut.props.email).toBeDefined()
     expect(sut.props.email).toEqual(props.email)
@@ -33,6 +46,19 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.password).toBeDefined()
     expect(sut.props.password).toEqual(props.password)
     expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Setter of password field', () => {
+    const pass = 'some password'
+    sut['password'] = pass
+    expect(sut.props.password).toEqual(pass)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Update Password field', () => {
+    const password = 'some password'
+    sut.updatePassword(password)
+    expect(sut.props.password).toEqual(password)
   })
 
   it('Getter of CreatedAt field', () => {
