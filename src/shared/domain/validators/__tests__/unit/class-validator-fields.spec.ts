63a946eb962a5fb945e2146c47d0b74b88c939e2
Luís Farias <luisgmfarias@gmail.com>
2024-12-26 18:10:48 -0300

    feat: add validators unit tests

--- src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
+++ src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -21,9 +21,21 @@ describe('ClassValidatorField unit tests', () => {
 
     const sut = new StubClassValidatorFields()
 
-    expect(sut.validate({ data: null })).toBeFalsy()
+    expect(sut.validate(null)).toBeFalsy()
     expect(spyValidateSync).toHaveBeenCalled()
     expect(sut.validatedData).toBeNull()
     expect(sut.errors).toStrictEqual({ field: ['test error'] })
+  })
+
+  it('Should validate without errors', () => {
+    const spyValidateSync = jest.spyOn(classValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([])
+
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.validate({ field: 'value' })).toBeTruthy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validatedData).toStrictEqual({ field: 'value' })
+    expect(sut.errors).toBeNull()
   })
 })
